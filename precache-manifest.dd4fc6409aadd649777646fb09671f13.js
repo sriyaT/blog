8a15231559005818a2d8cfb125c9188df00586a4
sriyaT <tsriya6@gmail.com>
2020-03-07 11:41:26 +0530

    Updates

---
+++ precache-manifest.dd4fc6409aadd649777646fb09671f13.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ebb9c57a405d4757cacff06119c88c6",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "e47d56c1b37658fa1eb7",
+    "url": "/blog/static/css/main.2d71bfb8.chunk.css"
+  },
+  {
+    "revision": "ab14319e1ada2d00782f",
+    "url": "/blog/static/js/2.bfb35ffa.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/blog/static/js/2.bfb35ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47d56c1b37658fa1eb7",
+    "url": "/blog/static/js/main.6b99a547.chunk.js"
+  },
+  {
+    "revision": "f36b6318bbc25180f3c7",
+    "url": "/blog/static/js/runtime-main.45f62c7f.js"
+  },
+  {
+    "revision": "659ac2854d5ad27a5a0165adc1b13afc",
+    "url": "/blog/static/media/BlogPostImage4.659ac285.jpeg"
+  },
+  {
+    "revision": "36a48f261c153345be4149649d870595",
+    "url": "/blog/static/media/BlogPostImage5.36a48f26.jpg"
+  },
+  {
+    "revision": "72f9f4ea987db1ba23854ebcdcc987b6",
+    "url": "/blog/static/media/blogpostimg1.72f9f4ea.jpg"
+  },
+  {
+    "revision": "ff795485ffda80818ce4d901b5edea6a",
+    "url": "/blog/static/media/fitness.ff795485.jpg"
+  },
+  {
+    "revision": "40e9380f129355462a838ae557190f83",
+    "url": "/blog/static/media/picu.40e9380f.jpg"
+  },
+  {
+    "revision": "0843c0b3a3ea2a670cb2d19cf4a5f30d",
+    "url": "/blog/static/media/sriyuu.0843c0b3.jpg"
+  }
+]);
